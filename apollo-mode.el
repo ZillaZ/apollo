0c3fdb7b55fa7dd3c1b2773f8ede839e2e02521b
Lucas Henrique <lucasmula125@gmail.com>
2024-07-22 19:30:36 +0000

    Better elisp code formatting and better Apollo example

--- apollo-mode.el
+++ apollo-mode.el
@@ -1,17 +1,20 @@
+;;; package --- apollo-mode
+;;; Commentary:
+;;; Nothing here
+;;; Code:
+(defvar apollo-varname nil "Apollo varname.")
+(setq apollo-varname "\\(\\w\\|_\\)+[[:space:]]*\\(=\\|:\\)")
 
-(defvar apollo-varname nil "apollo varname")
-(setq apollo-varname "\\(\\w\\|_\\)+[[:space:]]+\\(=\\|:\\)")
-
-(defvar apollo-keywords nil "apollo keywords")
+(defvar apollo-keywords nil "Apollo keywords.")
 (setq apollo-keywords '("fn" "if" "else" "return" "trait" "struct" "use" "static" "dyn" "let"))
 
-(defvar apollo-types nil "apollo types")
-(setq apollo-types '("f1" "f2" "f4" "f8" "i1" "i2" "i4" "i8" "string" "array"))
+(defvar apollo-types nil "Apollo types.")
+(setq apollo-types '("f1" "f2" "f4" "f8" "i1" "i2" "i4" "i8" "string" "array" "bool"))
 
-(defvar apollo-functions nil "apollo functions")
-(setq apollo-functions "\\w+[[:space:]]*\(\\|\)")
+(defvar apollo-functions nil "Apollo functions.")
+(setq apollo-functions "\\(\\w\\|_\\)+[[:space:]]*\(\\|\)")
 
-(defvar apollo-fontlock nil "list for font-lock-defaults")
+(defvar apollo-fontlock nil "List for \"font-lock-defaults\".")
 (setq apollo-fontlock
       (let (xkeywords-regex xtypes-regex xfunctions-regex xvarname-regex)
 
@@ -28,7 +31,7 @@
 
 ;;;###autoload
 (define-derived-mode apollo-mode c-mode "apollo-mode"
-  "Major mode for editing the Apollo Programming Language"
+  "Major mode for editing the Apollo Programming Language."
 
   ;; code for syntax highlighting
   (setq font-lock-defaults '((apollo-fontlock))))
@@ -37,3 +40,5 @@
 
 ;; add the mode to the `features' list
 (provide 'apollo-mode)
+
+;;; apollo-mode.el ends here
